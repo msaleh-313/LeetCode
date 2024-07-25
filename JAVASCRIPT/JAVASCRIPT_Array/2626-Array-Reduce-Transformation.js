754689622f2bfb25e15e589c6d20d51fdb64926e
msaleh-313 <168048496+msaleh-313@users.noreply.github.com>
2024-07-25 19:03:39 +0500

    [2626] [Time Beats: 41.10%] [Memory Beats: 70.14%] - LeetPush

--- JAVASCRIPT/JAVASCRIPT_Array/2626-Array-Reduce-Transformation.js
+++ JAVASCRIPT/JAVASCRIPT_Array/2626-Array-Reduce-Transformation.js
@@ -5,13 +5,13 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let len=nums.length    
+    let len=nums.length
     if(len>0){
         for(let i=0;i<len;++i){
-            init=fn(init,nums[i]);
+            init=fn(init,nums[i])
         }
-        return init
     }
-    return init    
+    return init;
+
 }
 
