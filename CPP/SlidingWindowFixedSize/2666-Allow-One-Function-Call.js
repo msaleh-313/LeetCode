81f1ff2cc938648fb26df6fb16730729a337afd1
msaleh-313 <168048496+msaleh-313@users.noreply.github.com>
2024-07-27 18:42:50 +0500

    [2666] [Time Beats: 29.91%] [Memory Beats: 73.15%] - LeetPush

---
+++ CPP/SlidingWindowFixedSize/2666-Allow-One-Function-Call.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+
+    let called=false;
+    
+    return function(...args){
+        let result;
+        
+        if(!called){
+            result=fn(...args)
+            called=true;
+        }
+        return result;
+    }
+    
+    
+};
+
+/**
+ * let fn = function(a,b,c){
+     (a + b + c)
+    }
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
